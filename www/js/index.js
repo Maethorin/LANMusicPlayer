dc9cc25bf569358d4afb7d8c81bd98af72bfcaa5
Márcio Duarte <maethorin@gmail.com>
2014-12-16 15:34:34 -0200

    Não precisa dos plugins. Refatorando JS

--- www/js/index.js
+++ www/js/index.js
@@ -1,23 +1,5 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
-    // Application Constructor
     initialize: function() {
         this.bindEvents();
     },
@@ -28,127 +10,111 @@ var app = {
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
+    resetPlayer: function(audioUrl) {
+        this.audioURL = audioUrl;
+        this.player = new Audio(this.audioURL);
+    },
+    setVariables: function() {
+        this.resetPlayer('http://stream.4zzzfm.org.au:789/;');
+        this.isPlaying = false;
+        this.readyStateInterval = null;
+        this.playButton = document.getElementById('playbutton');
+        this.stopButton = document.getElementById('stopbutton');
+        this.textPosition = document.getElementById('textposition');
+    },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
+    // function, we must explicitly call 'app.receivedEvent(...);'w
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+        app.setVariables();
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
-
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
-
         console.log('Received Event: ' + id);
+    },
+    buttonPlayPressed: function() {
+        if (this.isPlaying) {
+            this.pause();
+        }
+        else {
+            this.play();
+        }
+    },
+    play: function () {
+        this.player.play();
+        app.changeState('play');
+        this.readyStateInterval = setInterval(function () {
+            if (app.player.readyState <= 2) {
+                app.textPosition.innerHTML = 'loading...';
+            }
+        }, 1000);
+        this.player.addEventListener("timeupdate", function () {
+            var s = parseInt(app.player.currentTime % 60);
+            var m = parseInt((app.player.currentTime / 60) % 60);
+            var h = parseInt(((app.player.currentTime / 60) / 60) % 60);
+            if (app.isPlaying && app.player.currentTime > 0) {
+                app.textPosition.innerHTML = pad2(h) + ':' + pad2(m) + ':' + pad2(s);
+            }
+        }, false);
+        this.player.addEventListener("error", function () {
+            console.log('this.player ERROR');
+        }, false);
+        this.player.addEventListener("canplay", function () {
+            console.log('this.player CAN PLAY');
+        }, false);
+        this.player.addEventListener("waiting", function () {
+            app.changeState('pause');
+        }, false);
+        this.player.addEventListener("playing", function () {
+            app.changeState('play');
+        }, false);
+        this.player.addEventListener("ended", function () {
+            app.stop();
+            if (window.confirm('Streaming failed. Possibly due to a network error. Retry?')) {
+                onConfirmRetry();
+            }
+        }, false);
+    },
+    pause: function () {
+        this.changeState('pause');
+        clearInterval(this.readyStateInterval);
+        this.player.pause();
+    },
+    stop: function () {
+        this.changeState('stop');
+        clearInterval(this.readyStateInterval);
+        this.player.pause();
+        this.textPosition.innerHTML = '';
+        this.resetPlayer('http://stream.4zzzfm.org.au:789/;');
+    },
+    changeState: function(state) {
+        this.isPlaying = state == 'play';
+        this.playButton.innerHTML = '<img src="file:///android_res/drawable/ic_button_play.png" />';
+        if (state == 'play') {
+            this.playButton.innerHTML = '<img src="file:///android_res/drawable/ic_button_pause.png" />';
+        }
     }
 };
 
 app.initialize();
 
-var progressTimer;
-
-var playButton;
-var stopButton;
-var activityIndicator;
-var textPosition;
-
-function onError(error)
-{
-	console.log(error.message);
+function onError(error) {
+    console.log(error.message);
 }
 
 function onConfirmRetry(button) {
-	if (button == 1) {
-		html5audio.play();
-	}
+    if (button == 1) {
+        app.play();
+    }
 }
 
 function pad2(number) {
-	return (number < 10 ? '0' : '') + number
+    return (number < 10 ? '0' : '') + number
 }
-
-var myaudioURL = 'http://stream.4zzzfm.org.au:789/;';
-var myaudio = new Audio(myaudioURL);
-var isPlaying = false;
-var readyStateInterval = null;
-
-var html5audio = {
-	play: function()
-	{
-		isPlaying = true;
-		myaudio.play();
-
-		readyStateInterval = setInterval(function(){
-			 if (myaudio.readyState <= 2) {
-				 playButton.style.display = 'none';
-				 activityIndicator.style.display = 'block';
-				 textPosition.innerHTML = 'loading...';
-			 }
-		},1000);
-		myaudio.addEventListener("timeupdate", function() {
-			 var s = parseInt(myaudio.currentTime % 60);
-			 var m = parseInt((myaudio.currentTime / 60) % 60);
-			 var h = parseInt(((myaudio.currentTime / 60) / 60) % 60);
-			 if (isPlaying && myaudio.currentTime > 0) {
-				 textPosition.innerHTML = pad2(h) + ':' + pad2(m) + ':' + pad2(s);
-			 }
-		}, false);
-		myaudio.addEventListener("error", function() {
-			 console.log('myaudio ERROR');
-		}, false);
-		myaudio.addEventListener("canplay", function() {
-			 console.log('myaudio CAN PLAY');
-		}, false);
-		myaudio.addEventListener("waiting", function() {
-			 //console.log('myaudio WAITING');
-			 isPlaying = false;
-			 playButton.style.display = 'none';
-			 stopButton.style.display = 'none';
-			 activityIndicator.style.display = 'block';
-		}, false);
-		myaudio.addEventListener("playing", function() {
-			 isPlaying = true;
-			 playButton.style.display = 'none';
-			 activityIndicator.style.display = 'none';
-			 stopButton.style.display = 'block';
-		}, false);
-		myaudio.addEventListener("ended", function() {
-			 //console.log('myaudio ENDED');
-			 html5audio.stop();
-			 // navigator.notification.alert('Streaming failed. Possibly due to a network error.', null, 'Stream error', 'OK');
-			 // navigator.notification.confirm(
-			 //	'Streaming failed. Possibly due to a network error.', // message
-			 //	onConfirmRetry,	// callback to invoke with index of button pressed
-			 //	'Stream error',	// title
-			 //	'Retry,OK'		// buttonLabels
-			 // );
-			 if (window.confirm('Streaming failed. Possibly due to a network error. Retry?')) {
-			 	onConfirmRetry();
-			 }
-		}, false);
-	},
-	pause: function() {
-		isPlaying = false;
-		clearInterval(readyStateInterval);
-		myaudio.pause();
-		stopButton.style.display = 'none';
-		activityIndicator.style.display = 'none';
-		playButton.style.display = 'block';
-	},
-	stop: function() {
-		isPlaying = false;
-		clearInterval(readyStateInterval);
-		myaudio.pause();
-		stopButton.style.display = 'none';
-		activityIndicator.style.display = 'none';
-		playButton.style.display = 'block';
-		myaudio = null;
-		myaudio = new Audio(myaudioURL);
-		textPosition.innerHTML = '';
-	}
-};
